3a0a459098bf25e075d350c563d41de6020d6c73
azfarus <samazfar52@gmail.com>
2023-06-06 22:14:22 +0600

    GetAxis Example Added

--- examples/GetAxis/GetAxis.ino
+++ examples/GetAxis/GetAxis.ino
@@ -1,10 +1,4 @@
-/* Get all possible data from MPU6050
- * Accelerometer values are given as multiple of the gravity [1g = 9.81 m/s²]
- * Gyro values are given in deg/s
- * Angles are given in degrees
- * Note that X and Y are tilt angles and not pitch/roll.
- *
- * License: MIT
+  /* Considers the vector [0 0 1]  and shows the angles with x y and z axes with the vector
  */
 
 #include "Wire.h"
@@ -16,17 +10,22 @@ unsigned long timer = 0;
 
 void setup() {
   Serial.begin(115200);
+ 
   Wire.begin();
   mpu.upsideDownMounting = true;
+  mpu.setBeta(4);
+  
   byte status = mpu.begin();
   Serial.print(F("MPU6050 status: "));
   Serial.println(status);
   while(status!=0){ } // stop everything if could not connect to MPU6050
-  mpu.setBeta(4);
+  
+  
   Serial.println(F("Calculating offsets, do not move MPU6050"));
   delay(1000);
   mpu.calcOffsets(true,false); // gyro and accelero
   Serial.println("Done!\n");
+  
   timer = micros();
 }
 
@@ -36,8 +35,8 @@ void loop() {
   mpu.MadgwickUpdate((float)(micros() - timer));
   timer = micros();
   Quaternion axis(0,0,0,1);
-  axis = Quaternion::multiply(mpu.getQuaternion(), axis);
-  axis = Quaternion::multiply(axis,mpu.getQuaternion().getInverse());
+  axis = Quaternion::multiply(mpu.getQuaternion(), axis);       
+  axis = Quaternion::multiply(axis,mpu.getQuaternion().getInverse()); // execute the p = qpq^-1 quaternion multiplication  
   float recip = 1.0/sqrt(axis.x*axis.x + axis.z*axis.z + axis.y*axis.y);
 
   float rad_deg = (180.0/3.1415);
